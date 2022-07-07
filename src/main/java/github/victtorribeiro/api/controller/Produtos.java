2be05fb0a7d9a23abf226e654c2d783ef67a51f9
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-07-06 22:54:44 -0300

    Adicionando a dependencia jpa e criando a entidade produtos

--- src/main/java/github/victtorribeiro/api/controller/Produtos.java
+++ src/main/java/github/victtorribeiro/api/controller/Produtos.java
@@ -1,4 +1,4 @@
-package github.victtorribeiro.controller;
+package github.victtorribeiro.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/github/victtorribeiro/domain/enums/TypeProducts.java
@@ -0,0 +1,8 @@
+package github.victtorribeiro.domain.enums;
+
+public enum TypeProducts {
+    CANECA_NORMAL,
+    CANECA_MAGICA,
+    CANECA_MAGICA_COLHER,
+    GARRAFA
+}
